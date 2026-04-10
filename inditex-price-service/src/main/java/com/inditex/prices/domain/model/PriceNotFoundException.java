33f1858bcd01111d820ce41c2bc289e1ecbd4016
GFT\gouc <gonzalo.llatser@gft.com>
2026-04-10 16:03:47 +0200

    refactor(project): remove dead code and tidy structure

--- inditex-price-service/src/main/java/com/inditex/prices/domain/model/PriceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.inditex.prices.domain.model;
-
-public class PriceNotFoundException extends RuntimeException {
-
-    public PriceNotFoundException(String message) {
-        super(message);
-    }
-}
